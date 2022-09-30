be6a33ce851e244c4bdcd56dac55fd8065701e49
ZionBlade <wernerct@mail.uc.edu>
2022-09-30 13:07:41 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+        double perimeter = 0;
+        String trash = "";
+
+        System.out.print("\nEnter the width of the rectangle: ");
+        width = in.nextDouble();
+
+        if(in.hasNextDouble())
+        {
+            
+        }
+    }
+}
